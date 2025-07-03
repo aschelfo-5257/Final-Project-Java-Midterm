cd220d894c358964d665e8a42343457f44f2d4ac
Aaron Schelfo <schelfoaaron918@gmail.com>
2025-07-03 02:02:41 -0400

    DaysOfWeek.java

--- Debugging Assignment /DaysOfWeek.java
+++ Debugging Assignment /DaysOfWeek.java
@@ -1,16 +1,20 @@
-package daysAndDates; 
+package daysAndDates;
 
-public class DaysOfWeek { 
+/**
+ * Utility class for converting numbers to day names.
+ */
+public class DaysOfWeek {
 
-    public static String dayOfWeekStr(int numberOfDay) {
-    String[] days = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-    if (numberOfDay < 1 || numberOfDay > 7) {
-        return "Invalid day";
-    }
-    return days[numberOfDay - 1];
     /**
      * Returns the name of the day for a given number (1=Sunday, ..., 7=Saturday).
      * @param numberOfDay the day number (1-7)
      * @return the name of the day, or "Invalid day" if out of range
      */
+    public static String dayOfWeekStr(int numberOfDay) {
+        String[] days = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+        if (numberOfDay < 1 || numberOfDay > 7) {
+            return "Invalid day";
+        }
+        return days[numberOfDay - 1];
+    }
 }

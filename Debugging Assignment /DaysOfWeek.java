faf956058ff13c5ccd41724d87f2b0c50d1b3190
Aaron Schelfo <schelfoaaron918@gmail.com>
2025-07-03 01:33:01 -0400

    DaysOfWeek.java

---
+++ Debugging Assignment /DaysOfWeek.java
@@ -0,0 +1,45 @@
+package daysAndDates; 
+
+public class DaysOfWeek { 
+
+    public static String dayOfWeekStr(int numberOfDay) { 
+
+        String dayStr = ""; 
+
+        switch (numberOfDay) { 
+
+            case 1: 
+                dayStr = "Sunday"; 
+                break; 
+
+            case 2: 
+                dayStr = "Monday"; 
+                break; 
+
+            case 3: 
+                dayStr = "Tuesday"; 
+                break; 
+
+            case 4: 
+                dayStr = "Wednesday"; 
+                break; 
+
+            case 5: 
+                dayStr = "Thursday"; 
+                break; 
+
+            case 6: 
+                dayStr = "Friday"; 
+                break; 
+
+            case 7: 
+                dayStr = "Saturday"; 
+                break; 
+            
+            default: 
+                dayStr = "Invalid day"; 
+
+        } 
+        return dayStr; 
+    } 
+} 

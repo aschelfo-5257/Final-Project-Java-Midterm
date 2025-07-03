d755fbfacd604509fa157b084cc46bee147501ff
Aaron Schelfo <schelfoaaron918@gmail.com>
2025-07-03 01:28:14 -0400

    TestDaysOfWeek.java

--- Debugging Assignment /TestDaysOfWeek.java
+++ Debugging Assignment /TestDaysOfWeek.java
@@ -1,43 +1,15 @@
-Inport daysAndDates.DaysOfWeek;
+import daysAndDates.DaysOfWeek; 
 
-public class TestDaysOfWeek {
+public class TestDaysOfWeek { 
 
-  public static void main(String[] args) {
-    System.out.println("Days of Week");
+	public static void main(String [] args) { 
 
-    for (int i = 0;i < 8;i++) {
-      System.out.println("Number: " + i + "\tDay Of Week ")
-    }
-  }
-}
+	  System.out.println(“Days of Week”); 
 
-package daysAndDates
+	  for (int i = 1; i <= 7; i++) { 
 
-public class DaysOfWeek {
-  public static String DayOfWeekStr(Int NumberOfDay) {
-    String dayStr = ""
-    switch (NumberOfDay) {
-      case 1:
-        dayStr = "Sunday";
-        break;
-      case 2:
-        dayStr = "Monday";
-        break;
-      case 3:
-        dayStr = "Tuesday";
-        break;
-      case 4:
-        dayStr = "Wednesday";
-        break;
-      case 5:
-        dayStr = "Thursday";
-        break;
-      case 6:
-        dayStr = "Friday";
-        break;
-      case 7:
-        dayStr = "Saturday";
-        break;
-    }
-  }
-}
+	  System.out.println(“Number: “ + i + “\tDay Of Week: ” + DaysOfWeek.dayOfWeekStr(i)); 
+
+    } 
+  } 
+} 

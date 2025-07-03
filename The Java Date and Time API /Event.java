5fb2fc203ace1b59621cfb4f456676cf16597afb
Aaron Schelfo <schelfoaaron918@gmail.com>
2025-07-02 20:07:57 -0400

    Event.java

--- The Java Date and Time API /Event.java
+++ The Java Date and Time API /Event.java
@@ -8,14 +8,14 @@ import java.time.Year;
  *
  */
 public class Event {
-    private int year;
+    private Year year;
 
     /**
      * Constructs an Event with the specified year.
      *
      * @param year the year of the event
      */
-    public Event(int year) {
+    public Event(Year year) {
         this.year = year;
     }
 
@@ -24,7 +24,7 @@ public class Event {
      *
      * @return the year of the event
      */
-    public int getYear() {
+    public Year getYear() {
         return this.year;
     }
 
@@ -33,25 +33,12 @@ public class Event {
      *
      * @param year the year to set
      */
-    public void setYear(int year) {
+    public void setYear(Year year) {
         this.year = year;
     }
-
-    public static void main(String[] args) {
-        // Get the current year
-        Year currentYear = Year.now();
-        System.out.println("Current Year: " + currentYear);
-
-        // Create a specific year
-        Year y2025 = Year.of(2025);
-        System.out.println("Year: " + y2025);
-
-        // Check if a year is a leap year
-        boolean isLeap = y2025.isLeap();
-        System.out.println("Is 2025 a leap year? " + isLeap);
-
-        // Parse a year from a String
-        Year parsedYear = Year.parse("1999");
-        System.out.println("Parsed Year: " + parsedYear);
-    }
+    /**
+     * Returns a string representation of the event.
+     *
+     * @return a string containing the year of the event
+     */
 }

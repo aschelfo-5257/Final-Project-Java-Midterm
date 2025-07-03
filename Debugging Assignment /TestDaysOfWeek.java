7f5c471d5a1e681ba7ef2787b38e45fd260cd079
Aaron Schelfo <schelfoaaron918@gmail.com>
2025-07-03 01:30:43 -0400

    TestDaysOfWeek.java

--- Debugging Assignment /TestDaysOfWeek.java
+++ Debugging Assignment /TestDaysOfWeek.java
@@ -1,15 +1,14 @@
-import daysAndDates.DaysOfWeek; 
+// Demonstrates mapping numbers 1-7 to days of the week
+import daysAndDates.DaysOfWeek;
 
-public class TestDaysOfWeek { 
+public class TestDaysOfWeek {
 
-	public static void main(String [] args) { 
+    public static void main(String[] args) {
+        System.out.println("Days of Week:");
 
-	  System.out.println(“Days of Week”); 
-
-	  for (int i = 1; i <= 7; i++) { 
-
-	  System.out.println(“Number: “ + i + “\tDay Of Week: ” + DaysOfWeek.dayOfWeekStr(i)); 
-
-    } 
-  } 
-} 
+        for (int i = 1; i <= 7; i++) {
+            // Assuming dayOfWeekStr(i) returns a valid day name for 1-7
+            System.out.println("Number: " + i + "\tDay Of Week: " + DaysOfWeek.dayOfWeekStr(i));
+        }
+    }
+}
